83f7e0352041f3be8891c1e2b3f8db776023a5d1
k-gn <rlarbska97@gmail.com>
2021-07-27 11:31:54 +0900

    no message

---
+++ design/src/com/company/design/Main.java
@@ -0,0 +1,8 @@
+package com.company.design;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ design/src/com/company/design/singleton/SocketClient.java
@@ -0,0 +1,4 @@
+package com.company.design.singleton;
+
+public class SocketClient {
+}
