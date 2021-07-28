9faa017dbb599286ce80c678af2796d08795f0cd
k-gn <rlarbska97@gmail.com>
2021-07-28 22:28:53 +0900

    no message

--- hello/src/main/java/com/example/hello/controller/ApiController.java
+++ hello/src/main/java/com/example/hello/controller/ApiController.java
@@ -1,4 +1,15 @@
 package com.example.hello.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // REST API 를 처리하는 컨트롤러로 등록
+@RequestMapping("/api") // URI 주소 할당
 public class ApiController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring boot";
+    }
 }
