f239e29334f3efa73869122ce7e0d4b80aa904cd
k-gn <rlarbska97@gmail.com>
2021-08-02 11:11:23 +0900

    no message

---
+++ validation/src/main/java/com/example/validation/validator/YearMonthValidator.java
@@ -0,0 +1,30 @@
+package com.example.validation.validator;
+
+import com.example.validation.annotation.YearMonth;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+// 검증용 클래스
+public class YearMonthValidator implements ConstraintValidator<YearMonth, String> {
+
+    private String pattern;
+
+    @Override
+    public void initialize(YearMonth constraintAnnotation) {
+        this.pattern = constraintAnnotation.pattern();
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        try {
+            // LocalDate은 기본적으로 dd까지 들어가야 한다.
+            LocalDate localDate = LocalDate.parse(value + "01", DateTimeFormatter.ofPattern(this.pattern));
+        }catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+}
