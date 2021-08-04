bc42a7237750e307005de81d942e5c03df60beb5
k-gn <rlarbska97@gmail.com>
2021-08-04 09:38:55 +0900

    async

--- async/src/main/java/com/example/async/AsyncApplication.java
+++ async/src/main/java/com/example/async/AsyncApplication.java
@@ -2,8 +2,10 @@ package com.example.async;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync // 비동기 적용
 public class AsyncApplication {
 
     public static void main(String[] args) {
